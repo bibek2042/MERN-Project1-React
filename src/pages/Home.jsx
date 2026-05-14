3b87b9ef2cada5f4f748461e82ace9dc3a43b946
bibek2042 <bibek2042@gmail.com>
2026-05-14 11:47:29 +0545

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import Card from "../components/Card"
+import Navbar from "../components/Navbar"
+
+function Home(){
+    return (
+        <>
+        <Navbar/><br/><br/><br/>
+
+        <div className ="flex flex-wrap">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        
+
+         </div>
+
+        </>
+    )
+}
+
+export default Home
