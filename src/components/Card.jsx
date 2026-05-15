beb292ce1a395e8fdb0896822e92ba5af7da06c4
bibek2042 <bibek2042@gmail.com>
2026-05-15 11:53:10 +0545

    get api

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
-function Card(){
+function Card({blog}){
     return(
       <>
 <div className="max-w-xs mx-auto overflow-hidden bg-white rounded-lg shadow-md">
     <div className="relative">
-        <img className="w-full h-64 object-cover" src="https://picsum.photos/200/200" alt="Image" />
+        <img className="w-full h-64 object-cover" src={'http://localhost:3000/' + blog.image} alt="Image" />
         <div className="absolute top-0 right-0">
             <div className="w-32 h-8 absolute top-4 -right-8">
                 <div
@@ -13,10 +13,9 @@ function Card(){
         </div>
     </div>
     <div className="p-4">
-        <h3 className="text-xl font-semibold mb-2">Product Sale</h3>
+        <h3 className="text-xl font-semibold mb-2">{blog.title}</h3>
         <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam bibendum
-            auctor felis, vel consequat odio ullamcorper eu.
+            {blog.description}
         </p>
     </div>
 </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,35 @@
+import { useEffect, useState } from "react"
 import Card from "../components/Card"
 import Navbar from "../components/Navbar"
+import axios from "axios"
 
 function Home(){
+    const [blogs,setBlogs] = useState([])    
+    const fetchBlogs =async()=>{
+     const response= await axios.get("http://localhost:3000/blog")
+     setBlogs(response.data.data)
+    }
+
+    useEffect(()=>{
+        fetchBlogs()
+    },[])
+
+    console.log(blogs)
+
     return (
         <>
         <Navbar/><br/><br/><br/>
 
         <div className ="flex flex-wrap">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        
-
+        {
+            blogs.map(function(blog){
+                return(
+                     <Card blog ={blog}/>
+                )
+            })
+        }
+          
+       
          </div>
 
         </>
